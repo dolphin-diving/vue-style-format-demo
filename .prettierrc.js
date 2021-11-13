ef12e70311c69e7248e9430d048e14d3dbc13877
liangzw7 <1901543053@qq.com>
2021-11-13 17:12:44 +0800

    添加prettierrc配置

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,4 +4,8 @@ module.exports = {
   singleQuote: true,
   trailingComma: 'es5',
   arrowParens: 'always',
+  // printWidth: 200,
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  htmlWhitespaceSensitivity: 'ignore',
 }
